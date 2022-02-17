ff7b9ded2a2b2534bdb9842e6a350598bc97df75
ebolainfluenza <6652415+ebolainfluenza@users.noreply.github.com>
2022-02-17 17:51:00 -0500

    include cycleDepth & image count

--- gol.sh
+++ gol.sh
@@ -3,8 +3,7 @@
 wc -l *.go *.js *.html
 go clean -i
 rm -rf images/*
-mkdir images
-go test -race
-go build -race
-go install -race
-time go run gol.go -r 64 -c 64 -x 620 -y 620
+go build -race # test, install
+time go run gol.go -r 64 -c 64 -x 800 -y 800 -d 7
+nimg=`ls -l images | wc -l`
+echo "images created: $nimg"
