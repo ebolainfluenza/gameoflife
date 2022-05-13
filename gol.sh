d72cab3df5a649b97b11aeaac37ff6a503da0296
ebolainfluenza <6652415+ebolainfluenza@users.noreply.github.com>
2022-05-13 11:50:48 -0400

    pedantic: use explicit bg, grid, & cell colors on CLI

--- gol.sh
+++ gol.sh
@@ -6,7 +6,8 @@ if [ ! -d images ]; then
     mkdir images
 fi
 rm -rf images/*
-go build -race # test, install
-time go run gol.go -r 64 -c 64 -x 800 -y 800 -d 7
-nimg=`ls -l images | wc -l`
-echo "images created: $nimg"
+go build -v -race # test, install
+go install -race
+time go run gol.go -r 64 -c 64 -x 620 -y 620 -d 7 -b 0x0 -g 0x0 -z 0xffff
+numimg=`ls -l images | wc -l`
+echo "$numimg images created"
